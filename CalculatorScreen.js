83a04dd17b0f16c9ea9dc72704dfa48cba8aa22f
Sakib Hossain <sakib199988@gmail.com>
2022-10-15 14:44:11 +0600

    first commit

---
+++ CalculatorScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View,Text}from 'react-native';
+export default class CalculatorScreen extends React.Component{
+    render(){
+        return(
+            <View style={{paddingTop:60}}>
+                <Text>Calculator Screen</Text>
+            </View>
+        );
+    }
+}
